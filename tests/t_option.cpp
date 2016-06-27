231720e21e5963428af24cccf80c959962e5ea8a
Qing Chang <connor@petabi.com>
2016-06-27 14:21:09 -0700

    Add CPPengine in option unit test

--- tests/t_option.cpp
+++ tests/t_option.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+#include "../src/CPPEngine.h"
 #include "../src/HyperscanEngine.h"
 #include "../src/PcapSource.h"
 #include "../src/PCRE2Engine.h"
@@ -11,29 +12,36 @@
 
 using namespace regexbench;
 
-void opTest(Engine *engine) {
+regexbench::MatchResult opTest(Engine *engine) {
   engine->compile(regexbench::loadRules(DATA_DIR "/rule/option.re"));
   size_t nsessions = 0;
   regexbench::PcapSource pcap(DATA_DIR "/pcap/option.pcap");
   auto match_info = buildMatchMeta(pcap, nsessions);
   engine->init(nsessions);
-  regexbench::MatchResult result = regexbench::match(*engine, pcap, 1, match_info);
-  ATF_REQUIRE_EQ(3, result.nmatches);
+  return regexbench::match(*engine, pcap, 1, match_info);
 }
 
 ATF_TEST_CASE_WITHOUT_HEAD(t_option);
 ATF_TEST_CASE_BODY(t_option) {
   RE2Engine re2engine;
-  opTest(&re2engine);
+  auto result = opTest(&re2engine);
+  ATF_REQUIRE_EQ(3, result.nmatches);
 
   HyperscanEngine hsengine;
-  opTest(&hsengine);
+  result = opTest(&hsengine);
+  ATF_REQUIRE_EQ(3, result.nmatches);
 
   REmatchAutomataEngine rengine;
-  opTest(&rengine);
+  result = opTest(&rengine);
+  ATF_REQUIRE_EQ(3, result.nmatches);
 
   PCRE2Engine pengine;
-  opTest(&pengine);
+  result = opTest(&pengine);
+  ATF_REQUIRE_EQ(3, result.nmatches);
+
+  CPPEngine cengine;
+  result = opTest(&cengine);
+  ATF_REQUIRE_EQ(2, result.nmatches);
 }
 
 ATF_INIT_TEST_CASES(tcs) {
