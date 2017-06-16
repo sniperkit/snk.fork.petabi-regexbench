1e9a1a514152cd820017e321b6bf80990fb9c0e4
Kwanghyun Kim <kwanghyunk@petabi.com>
2017-06-16 14:53:23 +0900

    Set up bases for pcre_check command line revamp

--- src/pcre_check/PcreChecker.cpp
+++ src/pcre_check/PcreChecker.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <vector>
 
+#include <boost/program_options.hpp>
 #include <hs/hs_compile.h>
 #include <hs/hs_runtime.h>
 #define PCRE2_CODE_UNIT_WIDTH 8
@@ -26,35 +27,71 @@ using std::vector;
 
 using regexbench::Rule;
 
-static void usage()
+namespace po = boost::program_options;
+
+static void usage(const po::options_description& pos,
+                  const po::options_description& opt)
 {
-  cerr << "arguments too few" << endl;
-  cerr << "command line should look like :" << endl;
-  cerr << endl;
-  cerr << "for direct result update ..." << endl;
-  cerr << "$ pcre_checker {db_file}" << endl;
-  cerr << endl;
-  cerr << "for shell access ..." << endl;
-  cerr << "$ pcre_checker -s" << endl;
+  cout << "Usage: pcre_checker <mode> options..." << endl;
+  cout << pos << endl << opt << endl;
 }
 
 static int runShell();
 
 int main(int argc, char** argv)
 {
-  if (argc < 2) {
-    usage();
-    return -1;
+  string mode;
+  string dbFile;
+  string jsonIn;
+  string jsonOut;
+  po::options_description posargs;
+  posargs.add_options()(
+      "mode", po::value<string>(&mode),
+      "mode to execute : 'shell' or 'command' (default to 'command')");
+  po::positional_options_description positions;
+  positions.add("mode", 1);
+  po::options_description optargs("Options");
+  optargs.add_options()("help,h", "Print usage information.");
+  optargs.add_options()("db,d", po::value<string>(&dbFile),
+                        "sqlite3 db file name to use");
+  optargs.add_options()("input,i", po::value<string>(&jsonIn),
+                        "json input file");
+  optargs.add_options()("output,o", po::value<string>(&jsonOut),
+                        "json output file");
+  po::options_description cliargs;
+  cliargs.add(posargs).add(optargs);
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv)
+                .options(cliargs)
+                .positional(positions)
+                .run(),
+            vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    usage(posargs, optargs);
+    std::exit(EXIT_SUCCESS);
   }
 
-  string arg(argv[1]);
+  if (vm.count("mode")) {
+    if (std::string("shell").find(mode) == 0) {
+      cout << "Entering shell mode" << endl;
+      return runShell();
+    } else if (std::string("command").find(mode) != 0) {
+      cout << "Unknown mode '" << mode << "' specified!!" << endl;
+      usage(posargs, optargs);
+      std::exit(EXIT_SUCCESS);
+    }
+  }
 
-  if (arg == "-s")
-    return runShell(); // Enter shell mode
+  //
+  // From this point on we are gonna do works related to command mode
+  //
 
-  // Now direct result update based on the existing database content
-
-  string dbFile(arg);
+  if (dbFile.empty()) {
+    cout << "db file name must be specified" << endl;
+    std::exit(EXIT_FAILURE);
+  }
 
   try {
     dbFile = "database=" + dbFile;
